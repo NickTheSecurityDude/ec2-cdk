d77ed3f9075dfe7a2d2914b7123e499aa080a8e9
NickTheSecurityDude <55202293+NickTheSecurityDude@users.noreply.github.com>
2020-12-21 18:36:08 -0600

    Add files via upload

--- stacks/ec2_stack.py
+++ stacks/ec2_stack.py
@@ -7,8 +7,14 @@ class EC2Stack(core.Stack):
   def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
    
-    ec2.Instance(self, "ec2-instance",
+    my_ec2=ec2.Instance(self, "ec2-instance",
       instance_type=ec2.InstanceType("t3.nano"),
       machine_image=ec2.LookupMachineImage(name="my-test-ami"),
       vpc=ec2.Vpc.from_lookup(self,"VPC",is_default=True)
     )
+
+    # allow SSH
+    my_ec2.connections.allow_from_any_ipv4(ec2.Port.tcp(22),"Internet access SSH")
+
+    # add inspector tag
+    core.Tags.of(my_ec2).add("inspector","True")
